3e00db1df92aa3e4f68191472472fbb1825e5c3b
wangfengye <1040441325@qq.com>
2018-11-20 14:52:50 +0800

    Demo添加修改运动模式的按钮

--- app/src/main/java/com/moxun/tagcloud/MainActivity.java
+++ app/src/main/java/com/moxun/tagcloud/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
+import android.widget.AdapterView;
 
 import com.moxun.tagcloudlib.view.TagCloudView;
 
@@ -52,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, FragmentTestActivity.class));
+            }
+        });
+        AppCompatSpinner spinner = (AppCompatSpinner) findViewById(R.id.test_mode);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 2:
+                        tagCloudView.setAutoScrollMode(TagCloudView.MODE_DISABLE);
+                        break;
+                    case 1:
+                        tagCloudView.setAutoScrollMode(TagCloudView.MODE_DECELERATE);
+                        break;
+                    case 0:
+                        tagCloudView.setAutoScrollMode(TagCloudView.MODE_UNIFORM);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
